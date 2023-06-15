f31dc73425c3aa1960807d1b06e4a7604b78cc7a
L <dr.trayaurus61@gmail.com>
2023-06-15 10:15:29 -0700

    main commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Flipside-Mod-Manager
+
+Flipside-Mod-Manager is a simple command-line tool for managing mods for the game Super Paper Mario. It provides an easy-to-use interface for installing, uninstalling, and backing up mods
+
+## Features
+
+1. **Mod Installation**: Install mods stored in a designated directory with just a few clicks.
+2. **Backup**: Backup your original game files before mod installation to ensure that you can always return to the original game.
+3. **Mod Uninstallation**: Remove all mods and restore the game to its original state.
+4. **Compiling Game ROM**: Option to compile your game ROM after mod installation.
+
+## How to Use
+
+### Prerequisites
+
+- Make sure you have the game Super Paper Mario in `.iso` or `.wbfs` format in the same directory as the Mod Manager.
+- All mods should be placed within a directory named `mods`, and each mod should be in its own folder within the `mods` directory.
+- Each mod directory must contain a `modinfo.ini` file. The first line of this file should be `modname=Your Mod Name`. Usually this is provided by the mod creator, but if not then you will have to make it yourself
+
+### Usage
+
+1. Run the Flipside-Mod-Manager application.
+2. If your game is not yet extracted, the tool will automatically extract it from `spm.iso` or `spm.wbfs` that it finds in the directory.
+3. You will see a list of available mods to install. Each mod will be numbered.
+4. To install a mod, simply enter the corresponding number and press `Enter`.
+5. If you want to uninstall all mods, enter `-1` and press `Enter`. This will restore the game to its original state using the backup.
+6. If you are finished and want to quit the application, enter `0` and press `Enter`. The program will then ask you if you want to compile your game rom. If you want to do this, enter `yes` and press `Enter`.
+
+**Disclaimer:** Modding can potentially damage game files. Always ensure that you have a backup of your original game files before installing any mods.
---
+++ src/main.cpp
@@ -0,0 +1,156 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <filesystem>
+#include <map>
+
+std::map < std::string, std::string > modPaths;
+
+bool checkExtracted() {
+  std::filesystem::path extractedPath("extracted");
+  std::filesystem::path isoPath("spm.iso");
+  std::filesystem::path wbfsPath("spm.wbfs");
+
+  if (std::filesystem::exists(extractedPath)) {
+    std::cout << "The extracted folder exists, reading contents...\n";
+  } else if (std::filesystem::exists(isoPath)) {
+    std::cout << "Extracting from spm.iso...\n";
+    int result = system("wit x spm.iso extracted");
+    if (result == -1) {
+      std::cerr << "Error occurred during extraction from spm.iso\n";
+      return false;
+    }
+  } else if (std::filesystem::exists(wbfsPath)) {
+    std::cout << "Extracting from spm.wbfs...\n";
+    int result = system("wit x spm.wbfs extracted");
+    if (result == -1) {
+      std::cerr << "Error occurred during extraction from spm.wbfs\n";
+      return false;
+    }
+  } else {
+    std::cout << "No extracted folder or iso could be found.\n";
+    return false;
+  }
+  return true;
+}
+
+std::string getModName(const std::string & modFolder) {
+  std::string line;
+  std::ifstream modinfo(modFolder + "/modinfo.ini");
+  if (modinfo.is_open()) {
+    std::getline(modinfo, line);
+    auto pos = line.find('=');
+    if (pos != std::string::npos) {
+      return line.substr(pos + 1);
+    }
+    modinfo.close();
+  } else {
+    std::cout << "Unable to open modinfo.ini file for mod: " << modFolder << std::endl;
+  }
+  return "";
+}
+
+void installMod(const std::string &modName) {
+  std::cout << "Installing " << modName << "...\n";
+
+  std::filesystem::path backupPath("backup");
+  if (!std::filesystem::exists(backupPath)) {
+    std::filesystem::create_directory(backupPath);
+  }
+
+  std::filesystem::path modPath = modPaths[modName];
+
+  for (const auto &entry : std::filesystem::recursive_directory_iterator(modPath)) {
+    if (entry.is_regular_file()) {
+      std::filesystem::path filePath = entry.path();
+      if (filePath.filename() != "modinfo.ini") {
+        std::filesystem::path relPath = std::filesystem::relative(filePath, modPath);
+        std::filesystem::path destination = "extracted/files/" / relPath;
+        if (std::filesystem::exists(destination)) {
+          std::filesystem::path backupDest = "backup/" / relPath;
+          std::filesystem::create_directories(backupDest.parent_path());
+          std::filesystem::rename(destination, backupDest);
+        }
+        std::filesystem::create_directories(destination.parent_path());
+        std::filesystem::copy(filePath, destination);
+      }
+    }
+  }
+
+  std::cout << "Installed " << modName << "\n";
+}
+
+void uninstallAllMods() {
+  std::cout << "Uninstalling all mods...\n";
+  std::filesystem::path backupPath("backup");
+  std::filesystem::path extractedPath("extracted/files");
+
+  if (std::filesystem::exists(backupPath)) {
+    for (const auto & entry: std::filesystem::recursive_directory_iterator(backupPath)) {
+      if (entry.is_regular_file()) {
+        std::filesystem::path filePath = entry.path();
+        std::filesystem::path relPath = std::filesystem::relative(filePath, backupPath);
+        std::filesystem::path destination = extractedPath / relPath;
+        std::filesystem::create_directories(destination.parent_path());
+        std::filesystem::copy(filePath, destination, std::filesystem::copy_options::overwrite_existing);
+      }
+    }
+    std::cout << "All mods uninstalled, original files restored.\n";
+  } else {
+    std::cout << "Backup folder doesn't exist, nothing to restore.\n";
+  }
+
+  std::filesystem::path modFolderPath = extractedPath / "mod";
+  if (std::filesystem::exists(modFolderPath)) {
+    std::filesystem::remove_all(modFolderPath);
+  }
+}
+
+
+int main() {
+  bool extracted = checkExtracted();
+
+  if (extracted == true) {
+    std::map < int, std::string > mods;
+    int modIndex = 1;
+
+    for (const auto & entry: std::filesystem::directory_iterator("./mods")) {
+      std::string modName = getModName(entry.path());
+      mods[modIndex] = modName;
+      modPaths[modName] = entry.path();
+      modIndex++;
+    }
+
+    while (true) {
+      std::cout << "\nMOD MANAGER\n\n";
+      for (const auto & mod: mods) {
+        std::cout << mod.first << ": " << mod.second << "\n";
+      }
+      std::cout << "Enter the number of the mod to install, -1 to uninstall all mods, or 0 to quit: ";
+      int choice;
+      std::cin >> choice;
+
+      if (choice == 0) {
+        std::string compileResponse;
+        std::cout << "Do you want to compile your game rom? (yes/no): ";
+        std::cin >> compileResponse;
+        if (compileResponse == "yes" || compileResponse == "Yes") {
+          std::cout << "Compiling your game rom...\n";
+          int result = system("wit copy --align-files extracted spm.wbfs");
+          if (result == -1) {
+            std::cerr << "Error occurred during compiling of the game rom\n";
+          }
+        }
+        break;
+      } else if (choice == -1) {
+        uninstallAllMods();
+      } else if (mods.find(choice) != mods.end()) {
+        installMod(mods[choice]);
+      } else {
+        std::cout << "Invalid choice.\n";
+      }
+    }
+  }
+  return 0;
+}
