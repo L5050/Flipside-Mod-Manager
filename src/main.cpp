8bd9d5e330406e88220a207483a82708d15cc1da
L <dr.trayaurus61@gmail.com>
2023-09-03 10:27:41 -0700

    beautified code

--- src/main.cpp
+++ src/main.cpp
@@ -4,41 +4,42 @@
 #include <string>
 #include <filesystem>
 #include <map>
+using namespace std;
 
-std::map < std::string, std::string > modPaths;
+map < string, string > modPaths;
 
-std::string readConfigFile() {
-  std::string line;
-  std::ifstream config("config.ini");
+string readConfigFile() {
+  string line;
+  ifstream config("config.ini");
   if (config.is_open()) {
-    std::getline(config, line);
+    getline(config, line);
     auto pos = line.find('=');
-    if (pos != std::string::npos) {
+    if (pos != string::npos) {
       return line.substr(pos + 1);
     }
     config.close();
   } else {
-    std::cout << "Unable to open config.ini\n";
+    cout << "Unable to open config.ini\n";
   }
   return "";
 }
-std::string gameVersion = readConfigFile();
+string gameVersion = readConfigFile();
 
-void writeConfigFile(const std::string & version) {
-  std::ofstream config("config.ini");
+void writeConfigFile(const string & version) {
+  ofstream config("config.ini");
   if (config.is_open()) {
     config << "version=" << version << "\n";
     config.close();
   } else {
-    std::cerr << "Unable to create config.ini\n";
+    cerr << "Unable to create config.ini\n";
   }
 }
 
-std::filesystem::path getFilesPath() {
-  std::filesystem::path dataPath("extracted/DATA/files");
-  std::filesystem::path defaultPath("extracted/files");
+filesystem::path getFilesPath() {
+  filesystem::path dataPath("extracted/DATA/files");
+  filesystem::path defaultPath("extracted/files");
 
-  if (std::filesystem::exists(dataPath)) {
+  if (filesystem::exists(dataPath)) {
     return dataPath;
   } else {
     return defaultPath;
@@ -46,216 +47,215 @@ std::filesystem::path getFilesPath() {
 }
 
 bool checkExtracted() {
-  std::filesystem::path extractedPath("extracted");
-  std::filesystem::path isoPath("spm.iso");
-  std::filesystem::path wbfsPath("spm.wbfs");
+  filesystem::path extractedPath("extracted");
+  filesystem::path isoPath("spm.iso");
+  filesystem::path wbfsPath("spm.wbfs");
 
-  if (std::filesystem::exists(extractedPath)) {
-    std::cout << "The extracted folder exists, reading contents...\n";
-  } else if (std::filesystem::exists(isoPath)) {
-    std::cout << "Extracting from spm.iso...\n";
+  if (filesystem::exists(extractedPath)) {
+    cout << "The extracted folder exists, reading contents...\n";
+  } else if (filesystem::exists(isoPath)) {
+    cout << "Extracting from spm.iso...\n";
     int result = system("wit x spm.iso extracted");
     if (result == -1) {
-      std::cerr << "Error occurred during extraction from spm.iso\n";
+      cerr << "Error occurred during extraction from spm.iso\n";
       return false;
     }
-  } else if (std::filesystem::exists(wbfsPath)) {
-    std::cout << "Extracting from spm.wbfs...\n";
+  } else if (filesystem::exists(wbfsPath)) {
+    cout << "Extracting from spm.wbfs...\n";
     int result = system("wit x spm.wbfs extracted");
     if (result == -1) {
-      std::cerr << "Error occurred during extraction from spm.wbfs\n";
+      cerr << "Error occurred during extraction from spm.wbfs\n";
       return false;
     }
   } else {
-    std::cout << "No extracted folder or iso could be found.\n";
+    cout << "No extracted folder or iso could be found.\n";
     return false;
   }
   return true;
 }
 
-std::string getModName(const std::string & modFolder, const std::string & gameVersion) {
+string getModName(const string & modFolder,
+  const string & gameVersion) {
   // Check for the game version folder before considering the mod valid
-  if (!std::filesystem::exists(modFolder + "/" + gameVersion)) {
+  if (!filesystem::exists(modFolder + "/" + gameVersion)) {
     return "";
   }
 
-  std::string line;
-  std::ifstream modinfo(modFolder + "/modinfo.ini");
+  string line;
+  ifstream modinfo(modFolder + "/modinfo.ini");
   if (modinfo.is_open()) {
-    std::getline(modinfo, line);
+    getline(modinfo, line);
     auto pos = line.find('=');
-    if (pos != std::string::npos) {
+    if (pos != string::npos) {
       return line.substr(pos + 1);
     }
     modinfo.close();
   } else {
-    std::cout << "Unable to open modinfo.ini file for mod: " << modFolder << std::endl;
+    cout << "Unable to open modinfo.ini file for mod: " << modFolder << endl;
   }
   return "";
 }
 
-void installMod(const std::string &modName) {
-  std::cout << "Installing " << modName << "...\n";
+void installMod(const string & modName) {
+  cout << "Installing " << modName << "...\n";
 
-  std::filesystem::path backupPath("backup");
-  std::filesystem::path extractedPath = getFilesPath();
+  filesystem::path backupPath("backup");
+  filesystem::path extractedPath = getFilesPath();
 
-  if (!std::filesystem::exists(backupPath)) {
-    std::filesystem::create_directory(backupPath);
+  if (!filesystem::exists(backupPath)) {
+    filesystem::create_directory(backupPath);
   }
 
-  std::filesystem::path modPath = modPaths[modName];
+  filesystem::path modPath = modPaths[modName];
 
-  for (const auto &entry : std::filesystem::recursive_directory_iterator(modPath)) {
+  for (const auto & entry: filesystem::recursive_directory_iterator(modPath)) {
     if (entry.is_regular_file()) {
-      std::filesystem::path filePath = entry.path();
+      filesystem::path filePath = entry.path();
       if (filePath.filename() != "modinfo.ini") {
-        std::filesystem::path relPath = std::filesystem::relative(filePath, modPath);
-        std::filesystem::path destination = extractedPath / relPath;
-        if (std::filesystem::exists(destination)) {
-          std::filesystem::path backupDest = backupPath / relPath;
-          if (!std::filesystem::exists(backupDest)) {  // Check if file already exists in backup
-            std::filesystem::create_directories(backupDest.parent_path());
-            std::filesystem::rename(destination, backupDest);
+        filesystem::path relPath = filesystem::relative(filePath, modPath);
+        filesystem::path destination = extractedPath / relPath;
+        if (filesystem::exists(destination)) {
+          filesystem::path backupDest = backupPath / relPath;
+          if (!filesystem::exists(backupDest)) { // Check if file already exists in backup
+            filesystem::create_directories(backupDest.parent_path());
+            filesystem::rename(destination, backupDest);
           }
         }
-        std::filesystem::create_directories(destination.parent_path());
-        std::filesystem::copy(filePath, destination, std::filesystem::copy_options::overwrite_existing);
+        filesystem::create_directories(destination.parent_path());
+        filesystem::copy(filePath, destination, filesystem::copy_options::overwrite_existing);
       }
     }
   }
 
-  std::cout << "Installed " << modName << "\n";
+  cout << "Installed " << modName << "\n";
 }
-
 
 void uninstallAllMods() {
-  std::cout << "Uninstalling all mods...\n";
-  std::filesystem::path backupPath("backup");
-  std::filesystem::path extractedPath = getFilesPath();
+  cout << "Uninstalling all mods...\n";
+  filesystem::path backupPath("backup");
+  filesystem::path extractedPath = getFilesPath();
 
-  if (std::filesystem::exists(backupPath)) {
-    for (const auto & entry: std::filesystem::recursive_directory_iterator(backupPath)) {
+  if (filesystem::exists(backupPath)) {
+    for (const auto & entry: filesystem::recursive_directory_iterator(backupPath)) {
       if (entry.is_regular_file()) {
-        std::filesystem::path filePath = entry.path();
-        std::filesystem::path relPath = std::filesystem::relative(filePath, backupPath);
-        std::filesystem::path destination = extractedPath / relPath;
-        std::filesystem::create_directories(destination.parent_path());
-        std::filesystem::copy(filePath, destination, std::filesystem::copy_options::overwrite_existing);
+        filesystem::path filePath = entry.path();
+        filesystem::path relPath = filesystem::relative(filePath, backupPath);
+        filesystem::path destination = extractedPath / relPath;
+        filesystem::create_directories(destination.parent_path());
+        filesystem::copy(filePath, destination, filesystem::copy_options::overwrite_existing);
       }
     }
-    std::cout << "All mods uninstalled, original files restored.\n";
+    cout << "All mods uninstalled, original files restored.\n";
   } else {
-    std::cout << "Backup folder doesn't exist, nothing to restore.\n";
+    cout << "Backup folder doesn't exist, nothing to restore.\n";
   }
 
-  std::filesystem::path modFolderPath = extractedPath / "mod";
-  if (std::filesystem::exists(modFolderPath)) {
-    std::filesystem::remove_all(modFolderPath);
+  filesystem::path modFolderPath = extractedPath / "mod";
+  if (filesystem::exists(modFolderPath)) {
+    filesystem::remove_all(modFolderPath);
   }
 }
 
+void uninstallMod(const string & modName) {
+  cout << "Uninstalling " << modName << "...\n";
 
-void uninstallMod(const std::string &modName) {
-  std::cout << "Uninstalling " << modName << "...\n";
+  filesystem::path backupPath("backup");
+  filesystem::path extractedPath = getFilesPath();
+  filesystem::path modPath = modPaths[modName];
+  filesystem::path modFolderPath = extractedPath / "mod";
 
-  std::filesystem::path backupPath("backup");
-  std::filesystem::path extractedPath = getFilesPath();
-  std::filesystem::path modPath = modPaths[modName];
-  std::filesystem::path modFolderPath = extractedPath / "mod";
-
-  if (std::filesystem::exists(backupPath)) {
-    for (const auto &entry : std::filesystem::recursive_directory_iterator(modPath)) {
+  if (filesystem::exists(backupPath)) {
+    for (const auto & entry: filesystem::recursive_directory_iterator(modPath)) {
       if (entry.is_regular_file()) {
-        std::filesystem::path filePath = entry.path();
-        std::filesystem::path relPath = std::filesystem::relative(filePath, modPath);
-        std::filesystem::path backupDest = backupPath / relPath;
-        if (std::filesystem::exists(backupDest)) {
-          std::filesystem::path destination = extractedPath / relPath;
+        filesystem::path filePath = entry.path();
+        filesystem::path relPath = filesystem::relative(filePath, modPath);
+        filesystem::path backupDest = backupPath / relPath;
+        if (filesystem::exists(backupDest)) {
+          filesystem::path destination = extractedPath / relPath;
           // Skip if destination is within modFolderPath
           if (destination.string().find(modFolderPath.string()) != 0) {
-            std::filesystem::copy(backupDest, destination, std::filesystem::copy_options::overwrite_existing);
+            filesystem::copy(backupDest, destination, filesystem::copy_options::overwrite_existing);
           }
         }
       }
     }
-    std::cout << "Mod " << modName << " uninstalled, original files restored.\n";
-    if (std::filesystem::exists(modFolderPath)) {
-      std::filesystem::remove_all(modFolderPath);
+    cout << "Mod " << modName << " uninstalled, original files restored.\n";
+    if (filesystem::exists(modFolderPath)) {
+      filesystem::remove_all(modFolderPath);
     }
   } else {
-    std::cout << "Backup folder doesn't exist, nothing to restore.\n";
+    cout << "Backup folder doesn't exist, nothing to restore.\n";
   }
 }
 
 int main() {
   if (gameVersion.empty()) {
-  std::cout << "Could not determine game version. Would you like to enter it manually? (yes/no): ";
-  std::string response;
-  std::cin >> response;
-  if (response == "yes" || response == "Yes") {
-    std::cout << "Please reopen Flipside Mod Manager after choosing your SPM version";
-    std::cout << "Examples of valid versions include US2, US0, JP0, EU0\n";
-    std::cout << "Please enter your SPM version: ";
-    std::cin >> gameVersion;
-    writeConfigFile(gameVersion);
+    cout << "Could not determine game version. Would you like to enter it manually? (yes/no): ";
+    string response;
+    cin >> response;
+    if (response == "yes" || response == "Yes") {
+      cout << "Please reopen Flipside Mod Manager after choosing your SPM version";
+      cout << "Examples of valid versions include US2, US0, JP0, EU0\n";
+      cout << "Please enter your SPM version: ";
+      cin >> gameVersion;
+      writeConfigFile(gameVersion);
 
-  } else {
-    std::cout << "Exiting.\n";
-    return 1;
+    } else {
+      cout << "Exiting.\n";
+      return 1;
+    }
   }
-}
 
-    bool extracted = checkExtracted();
+  bool extracted = checkExtracted();
 
-    if (extracted == true) {
-      std::map<int, std::string> mods;
-      int modIndex = 1;
+  if (extracted == true) {
+    map < int, string > mods;
+    int modIndex = 1;
 
-      for (const auto &entry: std::filesystem::directory_iterator("./mods")) {
-        std::string modName = getModName(entry.path(), gameVersion);  // Pass game version
-        if (!modName.empty()) {  // Only add the mod if the game version folder exists
-          mods[modIndex] = modName;
-          modPaths[modName] = entry.path() / gameVersion;  // Append game version to mod path
-          modIndex++;
-        }
+    for (const auto & entry: filesystem::directory_iterator("./mods")) {
+      string modName = getModName(entry.path(), gameVersion); // Pass game version
+      if (!modName.empty()) { // Only add the mod if the game version folder exists
+        mods[modIndex] = modName;
+        modPaths[modName] = entry.path() / gameVersion; // Append game version to mod path
+        modIndex++;
       }
+    }
 
     while (true) {
-      std::cout << "\nMOD MANAGER\n\n";
+      cout << "\nMOD MANAGER\n\n";
       for (const auto & mod: mods) {
-        std::cout << mod.first << ": " << mod.second << "\n";
+        cout << mod.first << ": " << mod.second << "\n";
       }
-      std::cout << "Enter the number of the mod to install, -1 to uninstall all mods, -2 followed by the mod number to uninstall a single mod, or 0 to quit: ";
+      cout << "Enter the number of the mod to install, -1 to uninstall all mods, -2 followed by the mod number to uninstall a single mod, or 0 to quit: ";
       int choice;
-      std::cin >> choice;
+      cin >> choice;
 
       if (choice == 0) {
-        std::string compileResponse;
-        std::cout << "Do you want to compile your game rom? (yes/no): ";
-        std::cin >> compileResponse;
+        string compileResponse;
+        cout << "Do you want to compile your game rom? (yes/no): ";
+        cin >> compileResponse;
         if (compileResponse == "yes" || compileResponse == "Yes") {
-          std::cout << "Compiling your game rom...\n";
+          cout << "Compiling your game rom...\n";
           int result = system("wit copy --align-files extracted spm.wbfs");
           if (result == -1) {
-            std::cerr << "Error occurred during compiling of the game rom\n";
+            cerr << "Error occurred during compiling of the game rom\n";
           }
         }
         break;
       } else if (choice == -1) {
         uninstallAllMods();
       } else if (choice == -2) {
-        std::cout << "Enter the number of the mod to uninstall: ";
-        std::cin >> choice;
+        cout << "Enter the number of the mod to uninstall: ";
+        cin >> choice;
         if (mods.find(choice) != mods.end()) {
           uninstallMod(mods[choice]);
         } else {
-          std::cout << "Invalid mod number.\n";
+          cout << "Invalid mod number.\n";
         }
       } else if (mods.find(choice) != mods.end()) {
         installMod(mods[choice]);
       } else {
-        std::cout << "Invalid choice.\n";
+        cout << "Invalid choice.\n";
       }
     }
   }
